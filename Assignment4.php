8f3b02ce745c58e6e6ad20f63f52c4edf7572649
Ibnulhaisan <60622364+Ibnulhaisan@users.noreply.github.com>
2023-10-03 22:03:02 +0600

    assignmentmodule-4

---
+++ Assignment4.php
@@ -0,0 +1,39 @@
+<?php
+
+class Product{
+   private $id; 
+private $name; 
+private $price; 
+public function __construct($id,$name,$price){
+    $this->id = $id;
+      $this->name = $name;
+      $this->price = $price;
+}
+
+public function getId(){
+    return $this->id;
+}
+
+ public function getName(){
+    return $this->name;
+}
+public function getPrice(){
+    return $this->price;
+}
+public function getFormattedPrice() {
+        return number_format($this->price, 2);
+    }
+public function showDetails()
+{
+    echo "Product Details :"."\n";
+    echo "ID:".$this->getId()."\n";
+    echo "Name:".$this->getName()."\n";
+    echo "Price:".$this->getPrice();
+}
+}
+
+$product = new Product(1,"Book",20);
+$product->showDetails();
+
+
+?>
